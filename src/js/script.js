95811c60607b8509f5aca7d291ff277e13493e0f
Langenfeld Louise <louise.langenfeld@edu.univ-fcomte.fr>
2025-11-12 17:10:39 +0100

    ouverture fermeture

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// On cible les éléments à modifier
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+
+// Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+  // Mise à jour des attributs ARIA pour accessibilité
+        toggle.ariaExpanded = isClosed;
+        nav.ariaHidden = isOpen;
+});
